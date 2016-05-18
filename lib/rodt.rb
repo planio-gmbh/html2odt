59a5bf4018b8ea79c848452ec840cfa25ac866cc
Gregor Schmidt <schmidt@nach-vorne.eu>
2016-05-18 13:18:26 +0200

    Make sure to define module before extending it
    This enables loading the code without rubygems/bundler, i.e. executing the gemspec, e.g. when executing a single test file.

--- lib/rodt.rb
+++ lib/rodt.rb
@@ -3,12 +3,12 @@ require "nokogiri"
 require "xml/xslt"
 require "zip"
 
-require "rodt/image"
-require "rodt/odt"
-require "rodt/version"
-
 module Rodt
   XHTML2ODT_XSL = File.join(File.dirname(__FILE__), "..", "xsl", "xhtml2odt.xsl")
   XHTML2ODT_STYLES_XSL = File.join(File.dirname(__FILE__), "..", "xsl", "styles.xsl")
   ODT_TEMPLATE  = File.join(File.dirname(__FILE__), "..", "odt", "template.odt")
 end
+
+require "rodt/image"
+require "rodt/odt"
+require "rodt/version"
